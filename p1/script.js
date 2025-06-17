d0ddd2139f7db314203c80738029e998c3478a50
deep7311 <deepakchoudhary73111@gmail.com>
2025-06-17 11:32:02 +0530

    updated

--- p1/script.js
+++ p1/script.js
@@ -1,5 +1,31 @@
+// creating global variable for storing the user
+let users = []
+
+const validateUser = () => {
+    let username = document.getElementById("login-username").value;
+    let password = document.getElementById("login-password").value;
+
+    // check karna hai ki user exist karta hai ya nahi
+    const foundUser = users.find(u => u.name === username && u.password === password);
+    
+    if (foundUser) {
+        showHome();
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
+}
+
 // create login form function
 const loginForm = () => {
+
+    let usersList = '';
+
+    if (users.length === 0) {
+        usersList = `<p>No users registered yet.</p>`;
+    } else {
+        usersList = `<ul>` + users.map(user => `<li>username: ${user.name} (password: ${user.email})</li>`).join('') + `</ul>`;
+    }
+
     const str = `<div>
         <h2>Login Form</h2>
         <p>
@@ -11,14 +37,35 @@ const loginForm = () => {
             <input type="password" id="login-password" placeholder="Enter Password">
         </p>
         <p>
-            <button onclick="showHome()">Login</button>
+            <button onclick="validateUser()">Login</button>
         </p>
         <p>
             <button onclick="registerForm()">Not an Account? Create Account</button>
         </p>
+
+        
+        
+    </div>
+    <div>
+        <h3>Registered Users:</h3>
+        ${usersList}
     </div>`;
 
     root.innerHTML = str;
+}
+
+// function to save user details
+// and add to users array
+const saveUser = () => {
+    let name = document.getElementById("register-username").value;
+    let email = document.getElementById("register-email").value;
+    let password = document.getElementById("register-password").value;
+    let user = {};
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    users.push(user);
+    loginForm();
 }
 
 // create register form function
@@ -38,7 +85,7 @@ const registerForm = () => {
             <input type="password" id="register-password" placeholder="Enter Password">
         </p>
         <p>
-            <button onclick="loginForm()">Submit</button>
+            <button onclick="saveUser()">Submit</button>
         </p>
         <p>
             <button onclick="loginForm()">Already an Account? Login here...</button>
